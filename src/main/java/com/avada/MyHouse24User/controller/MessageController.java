727d8332cc353013f17230a91014e6cd17e21d91
Denis Derkach <derkach.denis@lll.kpi.ua>
2023-08-04 22:29:48 +0300

    start message

---
+++ src/main/java/com/avada/MyHouse24User/controller/MessageController.java
@@ -0,0 +1,17 @@
+package com.avada.MyHouse24User.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/message")
+@RequiredArgsConstructor
+public class MessageController {
+    @GetMapping("/index")
+    public String getAll(){
+        return "view/message-get-all";
+    }
+//    @GetMapping("")
+}
