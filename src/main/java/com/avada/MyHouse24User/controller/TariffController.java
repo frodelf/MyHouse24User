b18e38f0e8c4312fcf95d1750735f3099703294f
Denis Derkach <derkach.denis@lll.kpi.ua>
2023-08-04 22:27:57 +0300

    add tariff

---
+++ src/main/java/com/avada/MyHouse24User/controller/TariffController.java
@@ -0,0 +1,21 @@
+package com.avada.MyHouse24User.controller;
+
+import com.avada.MyHouse24User.services.impl.FlatServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/tariff")
+public class TariffController {
+    private final FlatServiceImpl flatService;
+    @GetMapping("/{id}")
+    public String getById(@PathVariable("id")long id, Model model){
+        model.addAttribute("flat", flatService.getById(id));
+        return "view/tariff-get-by-id";
+    }
+}
