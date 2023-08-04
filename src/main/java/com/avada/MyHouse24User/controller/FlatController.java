6242308b91f73009f28c12f039988b3ebff0c008
Denis Derkach <derkach.denis@lll.kpi.ua>
2023-08-04 22:26:17 +0300

    add flat

--- src/main/java/com/avada/MyHouse24User/controller/FlatController.java
+++ src/main/java/com/avada/MyHouse24User/controller/FlatController.java
@@ -1,15 +1,21 @@
 package com.avada.MyHouse24User.controller;
 
+import com.avada.MyHouse24User.services.impl.FlatServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/flat")
+@RequiredArgsConstructor
 public class FlatController {
+    private final FlatServiceImpl flatService;
     @GetMapping("/{id}")
-    public String flat(@PathVariable("id")long id){
+    public String flat(@PathVariable("id")long id, Model model){
+        model.addAttribute("flat", flatService.getById(id));
         return "view/flat";
     }
 }
---
+++ src/main/java/com/avada/MyHouse24User/controller/GlobalControllerAdvice.java
@@ -0,0 +1,19 @@
+package com.avada.MyHouse24User.controller;
+
+import com.avada.MyHouse24User.entity.User;
+import com.avada.MyHouse24User.services.impl.UserServiceImpl;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@Log4j2
+@ControllerAdvice
+@RequiredArgsConstructor
+public class GlobalControllerAdvice {
+    private final UserServiceImpl userService;
+    @ModelAttribute("user")
+    public User user(){
+        return userService.getById(1);
+    }
+}
